ca09874de593dc4ea598f2a407eb04f4fe3db60c
osawa-koki <koki@osawa.dev>
2023-10-16 21:12:44 +0900

    アノテーションの追加。

--- test/models/my_pokemon_test.rb
+++ test/models/my_pokemon_test.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: my_pokemons
+#
+#  id         :integer          not null, primary key
+#  hp         :float
+#  level      :integer
+#  max_hp     :float
+#  name       :string
+#  nickname   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'test_helper'
 
 class MyPokemonTest < ActiveSupport::TestCase
