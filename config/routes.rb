0628fb946d152f52f8d4b1a56f741482989b780b
osawa-koki <koki@osawa.dev>
2023-10-16 21:32:34 +0900

    'dotenv-rails'の導入。

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,12 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :my_pokemons, only: %i[index create update destroy]
+
+      resources :settings do
+        collection do
+          get :envs
+        end
+      end
     end
   end
 end
